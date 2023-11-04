c507fd677f5235d6b8d892c9f21fdcf40d769ba5
odman <okerdem.13@gmail.com>
2023-11-04 17:46:46 +0300

    - - - - - - -
    add files
    
    - - - - - - -

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,95 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import "./App.css"
+import MovieCard from "./MovieCard.tsx"
+import SearchIcon from "./search.svg"
 
-function App() {
+import { useEffect, useState } from "react";
+
+const API_URL = "https://www.omdbapi.com?apikey=d71c9ae6"
+
+const App = () => {
+
+  const [movies, setMovies] = useState<string[]>([])
+  const [searchTerm, setSearchTerm] = useState('')
+  const [searchService, setSearchService] = useState<SearchService | null>(null)
+
+  useEffect(() => {
+    setSearchService(new SearchService())
+  }, [])
+
+  const searchMovies = async (title: string) => {
+    setMovies(await searchService?.search(title))
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <div className='app'>
+        <h1>MovieLand</h1>
+        <SearchBar searchTerm={searchTerm} onSearchTermChange={setSearchTerm} onSearchClick={() => searchMovies(searchTerm)} />
+        <MovieList movies={movies} />
+      </div>
+    </>
+  )
 }
 
-export default App;
+interface ISearchBarProps {
+  searchTerm: string
+  onSearchTermChange: (searchTerm: string) => void
+  onSearchClick: () => void
+}
+
+const SearchBar = ({ searchTerm, onSearchTermChange, onSearchClick }: ISearchBarProps) => {
+  const handleFormSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    onSearchClick()
+  }
+
+  return (
+    <form className='search' onSubmit={handleFormSubmit}>
+      <input
+        placeholder='Search for movies'
+        value={searchTerm}
+        onChange={(e) => { onSearchTermChange(e.target.value) }}
+      />
+      <img
+        src={SearchIcon}
+        alt='search'
+        onClick={onSearchClick}
+      />
+    </form>
+  )
+}
+
+interface IMovieListProps {
+  movies: any[]
+}
+
+const MovieList = ({ movies }: IMovieListProps) => {
+  return (
+    <>
+      {
+        movies?.length > 0
+          ? (
+            <div className='container'>
+              {movies.map((movie) => (<MovieCard movie={movie} />))}
+            </div>
+          ) : (
+            <div className='empty'>
+              <h2>No movies found</h2>
+            </div>
+          )
+      }
+    </>
+  )
+}
+
+class SearchService {
+  async search(title: string) {
+    const response = await fetch(`${API_URL}&s=${title}`)
+    const data = await response.json()
+    return data.Search
+  }
+}
+
+export default App
